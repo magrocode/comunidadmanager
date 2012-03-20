9a42112abb6a398e2c52fa633fedb2b92cac8ec1
Mario Espinoza <mario@magrocode.com>
2012-03-20 15:40:00 -0400

    Agregando css bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # retorna el titulo completo sobre una pagina
+  def full_title(page_title)
+    base_title = "Comunidad Manager"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
