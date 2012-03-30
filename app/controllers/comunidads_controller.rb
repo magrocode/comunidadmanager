cb943c5c2b01055e43114a2ff0de08e8e1e59f81
Mario Espinoza <mario@magrocode.com>
2012-03-29 23:00:35 -0300

    Permisos de usuarios

--- app/controllers/comunidads_controller.rb
+++ app/controllers/comunidads_controller.rb
@@ -1,4 +1,7 @@
 class ComunidadsController < ApplicationController
+  before_filter :signed_in_user, only: [:index, :edit, :update]
+  before_filter :correct_user, only: [:show]
+  #before_filter :admin_user, only: [:edit, :update]
   
   def new
     @comunidad = Comunidad.new
@@ -18,4 +21,19 @@ class ComunidadsController < ApplicationController
       render 'new'
     end
   end
+  
+   private
+  
+    #def signed_in_user
+    #  redirect_to signin_path, notice: "Por favor autentiquese." unless signed_in?      
+    #end
+    
+    def correct_user
+      @comunidad = Comunidad.find(params[:id])
+      redirect_to(root_path) unless current_user?(@comunidad)
+    end
+    
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
 end
--- app/models/comunidad.rb
+++ app/models/comunidad.rb
@@ -25,7 +25,7 @@ class Comunidad < ActiveRecord::Base
     
   validates :nombre, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true, length: { minimum: 6 }
 
