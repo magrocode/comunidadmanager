4f76cf2d2cab1bf8aa6e44b30a08ded96968ad60
Mario Espinoza <mario@magrocode.com>
2012-04-04 19:25:44 -0400

    cambiando unidades

--- app/controllers/unidads_controller.rb
+++ app/controllers/unidads_controller.rb
@@ -1,5 +1,5 @@
 class UnidadsController < ApplicationController
-  before_filter :signed_in_user, only: [:index, :edit]
+  before_filter :signed_in_user, only: [:index, :edit, :update]
   before_filter :correct_user, only: [:show]
   
   def index
@@ -26,9 +26,20 @@ class UnidadsController < ApplicationController
   end
   
   def edit
-    @unidad = Unidad.find(params[:comunidad][:id])
+    @unidad = Unidad.find(params[:id])
   end
       
+  def update
+    #@user = User.find(params[:id])
+    if @unidad.update_attributes(params[:comunidad][:unidad])
+       flash[:success] = "Unidad actualizada"
+       #sign_in @user
+       #redirect_to @unidad
+     else
+       render 'edit'
+    end
+  end
+  
   private
   
     def correct_user
