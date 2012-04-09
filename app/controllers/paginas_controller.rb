0cfb2311a5932d9be1265357165bfea3b91d911a
Mario Espinoza <mario@magrocode.com>
2012-04-09 17:23:14 -0400

    cambiando menu

--- app/controllers/paginas_controller.rb
+++ app/controllers/paginas_controller.rb
@@ -1,4 +1,14 @@
 class PaginasController < ApplicationController
+  before_filter :signed_in_user
+  
   def home
   end
+  
+  private
+  
+    def signed_in_user
+      # si el usuario esta logueado redirecciona a perfil de comunidad
+      redirect_to current_user if signed_in?      
+    end
+
 end
