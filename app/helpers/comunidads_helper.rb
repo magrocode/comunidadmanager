1a7407c168b623c5c5bfeb47cd9e2103ea000e6f
Mario Espinoza <mario@magrocode.com>
2012-05-01 22:37:48 -0400

    finalizando interfaces usuarios

--- app/helpers/comunidads_helper.rb
+++ app/helpers/comunidads_helper.rb
@@ -1,2 +1,21 @@
 module ComunidadsHelper
+
+	def current_comunidad=(comunidad)
+	    @current_comunidad = comunidad
+	end
+	
+	def current_comunidad
+		@current_comunidad  ||= current_user.comunidad
+	end
+	 
+	def current_comunidad?(comunidad)
+		comunidad == current_comunidad
+	end
+ 
+ private
+    
+    def clear_return_to
+      session.delete(:return_to)
+    end
+
 end
