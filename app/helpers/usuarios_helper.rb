39890cb933539dcc8c239f21a32822a05d7fabee
Mario Espinoza <mario@magrocode.com>
2012-05-23 17:10:56 -0400

    construyendo profile de usuario

--- app/helpers/usuarios_helper.rb
+++ app/helpers/usuarios_helper.rb
@@ -1,9 +1,10 @@
 module UsuariosHelper
 # Returns the Gravatar (http://gravatar.com/) for the given usuario.
-  def gravatar_for(usuario, options = { size: 50 })
+  def gravatar_for(usuario, options = { size: 50, addclass: '' })
     gravatar_id = Digest::MD5::hexdigest(usuario.email.downcase)
     size = options[:size]
+    addclass = options[:addclass]
     gravatar_url = "http://gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    image_tag(gravatar_url, alt: usuario.nombre, class: "gravatar")
+    image_tag(gravatar_url, alt: usuario.nombre, class: "gravatar #{addclass}")
   end
 end
