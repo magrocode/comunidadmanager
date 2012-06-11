af876c6c2ef84dc3d68fa62e9a617275a811d7d5
Mario Espinoza <mario@magrocode.com>
2012-06-11 15:27:00 -0400

    eliminando archivos de git

--- config/initializers/setup_mail.rb
+++
@@ -1,7 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-  :address  => "mail.magrocode.com",
-  :port  => 26,
-  :user_name  => "mario@magrocode.com",
-  :password  => "ma@12.DEV",
-  :authentication  => :login
-}
