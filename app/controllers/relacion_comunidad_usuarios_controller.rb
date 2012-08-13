a0d65897d08cc271062770c5b3690aaa0faa408d
Mario Espinoza <mario@magrocode.com>
2012-08-12 23:24:39 -0400

    agregando ajax a check

--- app/controllers/relacion_comunidad_usuarios_controller.rb
+++ app/controllers/relacion_comunidad_usuarios_controller.rb
@@ -6,14 +6,25 @@ class RelacionComunidadUsuariosController < ApplicationController
   def destroy
   end
 
-  def update
-    @permiso = RelacionComunidadUsuario.find(params[:relacion_comunidad_usuario][:id])
-    @usuario = Usuario.find(params[:relacion_comunidad_usuario][:usuario_id])
-    @comunidad = Comunidad.find(params[:relacion_comunidad_usuario][:comunidad_id])
+#  def update
+#    @permiso = RelacionComunidadUsuario.find(params[:relacion_comunidad_usuario][:id])
+#    @usuario = Usuario.find(params[:relacion_comunidad_usuario][:usuario_id])
+#    @comunidad = Comunidad.find(params[:relacion_comunidad_usuario][:comunidad_id])
+#
+#    if @permiso.update_attributes(params[:relacion_comunidad_usuario])
+#      flash[:success] = "Los permisos de #{@usuario.nombre} han sido actualizados!"
+#      redirect_to comunidad_usuarios_path(@comunidad)
+#      #render :nothing => true
+#    end  
+#  end
 
-    if @permiso.update_attributes(params[:relacion_comunidad_usuario])
-      flash[:success] = "Los permisos de #{@usuario.nombre} han sido actualizados!"
-      redirect_to comunidad_usuarios_path(@comunidad)
-    end  
+  def autorizar_administrador
+    @usuario = Usuario.find(params[:relacion_comunidad_usuario][:usuario_id])
+    current_comunidad.autorizar_administrador!(@usuario)
+
+    respond_to do |format|
+      format.html { redirect_to comunidad_usuarios_path(current_comunidad) }
+      format.js 
+    end
   end
 end
