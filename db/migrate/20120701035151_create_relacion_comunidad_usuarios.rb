51709f3385c9936691925e2a056832b07a913272
Mario Espinoza <mario@magrocode.com>
2012-08-22 00:05:14 -0400

    cambiando nombre index

--- db/migrate/20120701035151_create_relacion_comunidad_usuarios.rb
+++ db/migrate/20120701035151_create_relacion_comunidad_usuarios.rb
@@ -10,6 +10,6 @@ class CreateRelacionComunidadUsuarios < ActiveRecord::Migration
 
     add_index :relacion_comunidad_usuarios, :comunidad_id
     add_index :relacion_comunidad_usuarios, :usuario_id
-    add_index :relacion_comunidad_usuarios, [:comunidad_id, :usuario_id], unique: true
+    add_index :relacion_comunidad_usuarios, [:comunidad_id, :usuario_id], unique: true, name: "index_rel_comunidad_usuarios_on_comunidad_id_and_usuario_id"
   end
 end
